c2a75ded76bf71dc656538da1ade60ef7d6767e5
minhvo1 <minhvo199@gmail.com>
2022-05-24 04:33:23 +0000

    implement character counter color change when over char limit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
-  $('#tweet-text').on('input', function(event) {
-    $('.counter').text(140 - $(this).val().length);
+  $('#tweet-text').on('input', function() {
+    $(this).parent().find('.counter').text(140 - $(this).val().length);
+    if ($(this).parent().find('.counter').text() < 0) {
+      $(this).parent().find('.counter').css('color', 'red');
+    } else {
+      $(this).parent().find('.counter').css('color', '#191716');
+    }
   })
 });
