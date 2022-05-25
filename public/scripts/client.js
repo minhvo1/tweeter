f02e5e5a5f38be7ea3227ab33f2749a9e16c4a87
minhvo1 <minhvo199@gmail.com>
2022-05-25 00:58:11 +0000

    implement creation of tweet elements from data and appending multiple tweet elements

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,7 +4,8 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-function createTweetElement (tweetData) {
+//Create a new single tweet article element from data
+const createTweetElement = function(tweetData) {
   let $tweet = `
   <article class="article-tweet">
     <header class="header-tweet">
@@ -33,24 +34,42 @@ function createTweetElement (tweetData) {
   return $tweet;
 }
 
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
+//Append multiple tweet articles to main container
+const renderTweets = function() {
+  for (const obj of data) {
+    $('.container').append(createTweetElement(obj));
+  }
 }
 
-const $tweet = createTweetElement(tweetData);
+//Test data
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
+//driver code (temporary)
+
 $(document).ready(function() {
-  $('.container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  renderTweets(data);
 }); 
   
