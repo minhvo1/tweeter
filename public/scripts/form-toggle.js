c06af4fd232dd7d3c2358cd90101fd4633c9a7ac
minhvo1 <minhvo199@gmail.com>
2022-05-26 05:16:39 +0000

    implement responsive design

--- public/scripts/form-toggle.js
+++ public/scripts/form-toggle.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
+  // Toggle composer form
   $('.action').on('click', function() {
     $('.new-tweet').slideToggle('slow', function(){
     })
   })
+
+  // Change action button color on hover
   $('.action').on('mouseover', function() {
     $(this).find('.icon').css('color', '#ba5e0d');
     $(this).find('p').css('color', '#ba5e0d');
